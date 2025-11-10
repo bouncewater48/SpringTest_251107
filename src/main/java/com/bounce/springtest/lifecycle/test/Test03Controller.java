d6b57044c1765801aab7d3c650e27d16bbf31495
bounecwater <bouncewater48@gmail.com>
2025-11-10 18:57:13 +0900

    [강승훈] - lifecycle/Test03Controller.java 생성 - application.properties 1차 추가 - lifecycle/test03.html 생성 - build.gradle 1차 추가

---
+++ src/main/java/com/bounce/springtest/lifecycle/test/Test03Controller.java
@@ -0,0 +1,13 @@
+package com.bounce.springtest.lifecycle.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Test03Controller {
+
+    @RequestMapping("/lifecycle/test03")
+    public String thymeleaf() {
+        return "lifecycle/test03";
+    }
+}
