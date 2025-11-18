262b988e8358f0b843341db2e3da75efc95baba0
bounecwater <bouncewater48@gmail.com>
2025-11-18 18:47:45 +0900

    [강승훈] - weatherhistory 생성

--- src/main/java/com/bounce/springtest/thymeleaf/WeatherController.java
+++ src/main/java/com/bounce/springtest/thymeleaf/WeatherController.java
@@ -1,4 +1,22 @@
 package com.bounce.springtest.thymeleaf;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/thymeleaf/weather")
+@Controller
 public class WeatherController {
+
+    @GetMapping("/list")
+    public String list() {
+        return "thymeleaf/weatherhistory/list";
+    }
+
+    @GetMapping("/form")
+    public String form() {
+        return "thymeleaf/weatherhistory/form";
+    }
+
+
 }
